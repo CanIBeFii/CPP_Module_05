8b7bfcab3ff6768e504c2c0b8a22362bccef4e32
CanIBeFii <filipe250502@gmail.com>
2023-09-25 19:02:26 +0100

    ex00: done main.cpp

--- ex00/includes/Bureaucrat.hpp
+++ ex00/includes/Bureaucrat.hpp
@@ -18,8 +18,8 @@ class Bureaucrat {
 
 		// Functions
 		//Getters
-		const std::string&	getName( void );
-		int					getGrade( void );
+		const std::string&	getName( void ) const;
+		int					getGrade( void ) const;
 
 		void				incrementGrade( void );
 		void				decrementGrade( void );
@@ -40,5 +40,13 @@ class Bureaucrat {
 };
 
 std::ostream&	operator<<( std::ostream& out, const Bureaucrat& b );
+
+
+// Colors
+#define RESET   "\033[0m"
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
 
 #endif
